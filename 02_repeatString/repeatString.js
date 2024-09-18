3859f41c5182675dbb38ff9acb06acc6747e93f8
homlandah <174951779+homlandah@users.noreply.github.com>
2024-09-18 16:43:15 +0300

    Modify as per exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+function repeatString(stringParameter, num) {
+    let repeatedString = "";
+    
+    for (let i = 1; i<=num; i++) {
+        repeatedString = repeatedString + stringParameter;
+    };
 
+    return repeatedString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
