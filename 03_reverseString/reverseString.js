0bdae78429360673d7c7141a200e6cea72457c04
homlandah <174951779+homlandah@users.noreply.github.com>
2024-09-26 14:13:27 +0300

    Modify as per exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+function reverseString(string) {
+    let reversedString = "";
+    const length = string.length;
+    for (i = length - 1; i >= 0; i--) {
+        reversedString = reversedString + string[i];
+    }
+    return reversedString
+}
 
-};
+
+// hello 
+
 
 // Do not edit below this line
 module.exports = reverseString;
