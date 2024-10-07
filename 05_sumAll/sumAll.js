f8bdcd3ce982f47f0cbd3c01af467ca2a6fdb114
homlandah <174951779+homlandah@users.noreply.github.com>
2024-10-07 17:01:09 +0300

    Modify as per exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
 
-};
+function sumAll(a, b) {
+    let finalSum = 0;
+
+    if ((a >= 0) && (b >= 0) && (Number.isInteger(a) === true) && (Number.isInteger(b) === true)) {
+        if (a < b) {
+            for (let i = a; i <= b; i++) {
+                finalSum = finalSum + i;
+            }
+        } else if (a > b) {
+            for (let i = a; i >= b; i--) {
+                finalSum = finalSum + i;
+            }
+        }
+    return finalSum;
+    } else {
+        return "ERROR";
+    } 
+}
 
 // Do not edit below this line
 module.exports = sumAll;
