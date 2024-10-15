720460971df0caa4553870637ae9e4815d83f3ec
homlandah <174951779+homlandah@users.noreply.github.com>
2024-10-15 15:25:36 +0300

    Modify as per exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
-};
+function leapYears(year) {
+    if (
+        (Number.isInteger(year)) && 
+        (year % 4 === 0) && 
+        ((year % 100 !== 0) || (year % 400 === 0))
+    ) {
+        return true
+    } else {
+        return false
+    };
+}
 
 // Do not edit below this line
 module.exports = leapYears;
